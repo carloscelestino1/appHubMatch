77627968dcb64ba17a54e43bf798c0da0a93fda6
Arthur Henrique <arthurnam@outlook.com>
2023-03-27 10:35:23 -0300

    commit

--- appHubMatch/configuracoes.py
+++ appHubMatch/configuracoes.py
@@ -1,3 +1,4 @@
+
 from kivymd.uix.card import MDCard
 from kivymd.app import MDApp
 from kivy.lang import Builder
@@ -5,86 +6,114 @@ from kivy.uix.screenmanager import Screen, ScreenManager
 from kivy.core.window import Window
 
 
-Window.size = (350, 580)
 
+Window.size = (350, 580)
 
 Builder.load_string('''
 
- 
 
 <JanelaPrincipal>:
     name: 'janelaprincipal'
     MDBoxLayout:
         orientation: 'vertical'
+       
         MDTopAppBar:
-            size_hint: 1, .1
+            size_hint: 1, .11
             elevation: 0
-            md_bg_color: "49C388"
-            anchor_title: "left"
-            right_action_items: [["bell-outline"]]
+            md_bg_color: "1111111"
+            anchor_title: "right"
+            title: "Ajustes"
+            specific_text_color: "black"
         MDFloatLayout:
-            Image:
-                source: "img/image4.png"
-                pos_hint: {'center_x': 0.5,'center_y': 0.80}
-                size_hint: 0.65, 0.33  
             MDTextField:              
                 hint_text: "Pesquisar"
                 mode: "round"
                 icon_left: "magnify"
-                pos_hint: {'center_x': .5,'center_y': .58}
+                pos_hint: {'center_x': .5,'center_y': .96}
                 size_hint: 0.70, 0.07
 
             MDCard:
-                id: 'Azul'
                 elevation: 0
                 md_bg_color:  0,0,0,0
                 spacing: 15
                 padding: 17
-                size_hint: 0.7, 0.25
-                pos_hint: {'center_x': .5,'center_y': 0.44}
+                size_hint: 1, 0.12
+                pos_hint: {'center_x': .5,'center_y': .89 }
                 
                 MDCard:
-                    id: ''
                     elevation: 0
-                    md_bg_color: "49C388"
                     ripple_behavior: True
-                    Image:
-                        source: "img/book.png"
-                        elevation: 0
-                MDCard:
-                    id: ''
-                    elevation: 0
-                    md_bg_color: "49C388"
-                    ripple_behavior: True
-                    Image:
-                        source: "img/money.png"
-                        elevation: 0
-                        
-                    
+                    MDLabel:
+                        adaptive_size: True
+                        text: 'Notificações'
+
             MDCard:
                 elevation: 0
                 md_bg_color:  0,0,0,0
                 spacing: 15
                 padding: 17
-                size_hint: 0.7, 0.25
-                pos_hint: {'center_x': .5,'center_y': 0.23}
+                size_hint: 1, 0.12
+                pos_hint: {'center_x': .5,'center_y': .81}
                 
                 MDCard:
-                    id: ''
                     elevation: 0
-                    md_bg_color: "49C388"
                     ripple_behavior: True
-                    Image:
-                        source: "img/music.png"
-                        elevation: 0
+                    MDLabel:
+                        adaptive_size: True
+                        text: 'Privacidade'
+            MDCard:
+                elevation: 0
+                md_bg_color:  0,0,0,0
+                spacing: 15
+                padding: 17
+                size_hint: 1, 0.12
+                pos_hint: {'center_x': .5,'center_y': .73}
+                
                 MDCard:
-                    id: ''
                     elevation: 0
-                    md_bg_color: "49C388"
                     ripple_behavior: True
-                    Image:
-                        source:"img/gast.png"
-                        elevation: 0
+                    MDLabel:
+                        adaptive_size: True
+                        text: 'Segurança'                                     
+            MDCard:
+                elevation: 0
+                md_bg_color:  0,0,0,0
+                spacing: 15
+                padding: 17
+                size_hint: 1, 0.12
+                pos_hint: {'center_x': .5,'center_y': .65}
+                
+                MDCard:
+                    elevation: 0
+                    ripple_behavior: True
+                    MDLabel:
+                        adaptive_size: True
+                        text: 'Anúncios'     
+            MDCard:
+                elevation: 0
+                md_bg_color:  0,0,0,0
+                spacing: 15
+                padding: 17
+                size_hint: 1, 0.12
+                pos_hint: {'center_x': .5,'center_y': .57}
+                
+                MDCard:
+                    elevation: 0
+                    ripple_behavior: True
+                    MDLabel:
+                        adaptive_size: True
+                        text: 'Conta'                           
+            MDFloatLayout:
+                Image:
+                    size_hint: 1, 0.01
+                    source: "img/line.png"
+                    pos_hint: {'center_x': .5,'center_y': .5}
+
+                Image:
+                    size_hint: 0.65, 0.33  
+                    source: "img/fada.png"
+                    pos_hint: {'center_x': .5,'center_y': .3}
+
             MDBottomNavigation:
                 panel_color: "49C388"
                 size_hint: 1, .06
@@ -99,6 +128,17 @@ Builder.load_string('''
                     icon:"forum"                    
                 MDBottomNavigationItem:
                     icon:"account-outline"
+
+                    
+
+
+
+
+
+
+
+
+
 <Janela1>:
     name: 'janela1'
     MDBoxLayout:
